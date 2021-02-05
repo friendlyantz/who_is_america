cea18026e463280531e579abc2ac0823fce32197
Anton <panteleev23@gmail.com>
2021-02-06 08:25:46 +1100

    politian has many quotes model

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,4 +1,4 @@
 class Politician < ApplicationRecord
   belongs_to :location, class_name: "Location", foreign_key: "location_id"
-  has_many :quotes, class_name: "Quote", foreign_key: "quotes_id"
+  has_many :quotes
 end
