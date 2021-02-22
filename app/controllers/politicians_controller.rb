0373b4d0c6b0bd5a59a3078c899adea6fa33fc91
Anton <panteleev23@gmail.com>
2021-02-23 07:20:50 +1100

    spacer

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -7,7 +7,6 @@
   def show
     find_politician
   end
-<<<<<<< HEAD
   
   private
   
@@ -18,7 +17,5 @@
   # def politician_params
   #   params.require(:politician).permit(:first_name, :last_name)    
   # end
-=======
->>>>>>> 5d6b991f62c8b486fee6acb485b79c0abdd2da75
 
 end
